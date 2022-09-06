23594c29231897a17ed0c2b604059898a9ba0aa6
JuliannaLazareva <juliannalazareva123@gmail.com>
2022-09-06 17:47:21 +0300

    часть проекта

--- source/js/main.js
+++ source/js/main.js
@@ -8,6 +8,68 @@ window.addEventListener('DOMContentLoaded', () => {
   // Utils
   // ---------------------------------
 
+  //const acc = document.querySelectorAll(".accordion__item");
+
+  //for (let i = 0; i < acc.length; i++) {
+  //  acc[i].addEventListener("click", function() {
+      /* Переключение между добавлением и удалением класса "accordion__item_show",
+      чтобы выделить кнопку, управляющую панелью */
+  //    this.classList.toggle("accordion__item_show");
+
+      /* Переключение между скрытием и отображением активной панели */
+    //  const panel = this.document.querySelectorAll(".accordion__body");
+      //if (panel.style.display === "block") {
+        //panel.style.display = "none";
+    //  } else {
+      //  panel.style.display = "block";
+      //}
+    //});
+  //}
+
+  const acc = document.querySelectorAll(".accordion__item");
+  const panel = document.querySelectorAll(".accordion__body");
+
+  for (let i = 0; i < acc.length; i++) {
+      acc[i].onclick = function() {
+        const setClasses = !this.classList.contains('accordion__item_show');
+          setClass(acc, 'accordion__item_show', 'remove');
+          setClass(panel, 'show', 'remove');
+
+           if (setClasses) {
+              this.classList.toggle("accordion__item_show");
+              this.nextElementSibling.classList.toggle("show");
+          }
+      }
+  }
+
+  function setClass(els, className, fnName) {
+      for (let i = 0; i < els.length; i++) {
+          els[i].classList[fnName](className);
+      }
+  }
+
+  //Функция открытия текста по клику на кнопку "Подробнее"
+  const btnText = document.querySelector(".about-company__button");
+  const breakText = document.querySelector(".about-company__break-text");
+  const moreTextMobile = document.querySelector(".about-company__more--mobile-only");
+  const moreTextTablet = document.querySelector(".about-company__more--tablet");
+
+  function readMore() {
+    if (breakText.style.display === "none") {
+      breakText.style.display = "inline";
+      btnText.innerHTML = "Подробнее";
+      moreTextMobile.style.display = "none";
+      moreTextTablet.style.display = "none";
+    }  else {
+      breakText.style.display = "none";
+      btnText.innerHTML = "Свернуть";
+      moreTextMobile.style.display = "inline";
+      moreTextTablet.style.display = "inline";
+    }
+  }
+
+  btnText.addEventListener('click', readMore);
+
   iosVhFix();
 
   // Modules
